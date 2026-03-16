3d4e805c1297d7d025e9d55cfc83d1a0927e4f65
beefysalad <jpatrickzephyr@gmail.com>
2026-03-17 01:46:15 +0800

    chore: move swagger to plugin

---
+++ src/plugins/swagger.ts
@@ -0,0 +1,19 @@
+import swagger from "@fastify/swagger";
+import swaggerUI from "@fastify/swagger-ui";
+import type { FastifyInstance } from "fastify";
+import { jsonSchemaTransform } from "fastify-type-provider-zod";
+
+export async function swaggerPlugin(app: FastifyInstance) {
+  await app.register(swagger, {
+    openapi: {
+      info: {
+        title: "Fastify Sales API",
+        version: "1.0.1",
+        description: "Sales data API for the 1-week backend challenge",
+      },
+    },
+    transform: jsonSchemaTransform,
+  });
+
+  await app.register(swaggerUI, { routePrefix: "/api/docs" });
+}
